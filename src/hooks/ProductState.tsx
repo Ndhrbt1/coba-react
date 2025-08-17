53d100f875f81ef93651ed0a059d540a2d95838a
Ndhrbt1 <indahroad04@gmail.com>
2025-08-17 11:02:35 +0700

    feat: read data from json (async)

--- src/hooks/ProductState.tsx
+++ src/hooks/ProductState.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import type { Product } from "../models/product";
-import productJson from "../assets/products.json";
+// import productJson from "../assets/products.json";
 
 function ProductState() {
   const [products, setProducts] = useState<Product[]>([]);
-  const fetchData = () => {
-    setProducts(productJson as Product[]);
+  const fetchData = async () => {
+    // setProducts(productJson as Product[]);
+    const data = await fetch("/products.json");
+    const productJson: Product[] = await data.json();
+    setProducts(productJson);
   };
 
   return { products, setProducts, fetchData };
