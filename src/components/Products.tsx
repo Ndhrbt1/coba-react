5164b15a00d94bd80905ed032bba38dd40146787
Ndhrbt1 <indahroad04@gmail.com>
2025-08-17 11:25:42 +0700

    feat: add data json

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,13 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ProductState from "../hooks/ProductState";
+import type { Product } from "../models/product";
 
 function Products() {
-  const { products, fetchData } = ProductState();
-
   useEffect(() => {
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const { products, fetchData, setProducts } = ProductState();
+
+  const [newProduct, setNewProduct] = useState("");
+
+  const addProduct = () => {
+    const newObjProduct: Product = {
+      id: crypto.randomUUID().slice(0, 5),
+      name: newProduct,
+      price: Math.floor(Math.random() * 10) + 10000,
+    };
+    setProducts([newObjProduct, ...products]);
+    setNewProduct("");
+  };
+
   return (
     <>
       <h1>List of Product</h1>
@@ -18,6 +32,18 @@ function Products() {
           </li>
         ))}
       </ul>
+      <br />
+
+      <input
+        type="text"
+        className="form-control"
+        value={newProduct}
+        onChange={(e) => setNewProduct(e.target.value)}
+      />
+      <br />
+      <button type="button" className="btn btn-primary" onClick={addProduct}>
+        add product
+      </button>
     </>
   );
 }
