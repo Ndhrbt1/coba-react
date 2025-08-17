c8f8f3088e93a71d8c1a106728e0c268f2921521
Ndhrbt1 <indahroad04@gmail.com>
2025-08-17 12:23:02 +0700

    feat: update data json

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -26,25 +26,48 @@ function Products() {
     setProducts(products.filter((value) => value.id != idProduct));
   };
 
+  const [changeProduct, setChangeProduct] = useState("");
+  const updateProduct = (idProduct: string) => {
+    setProducts(
+      products.map((value) =>
+        idProduct == value.id
+          ? {
+              id: value.id,
+              name: changeProduct,
+              price: value.price,
+            }
+          : value
+      )
+    );
+
+    setChangeProduct("");
+  };
+
   return (
     <>
       <h1>List of Product</h1>
       <ul className="list-group">
         {products.map((value) => (
           <li key={value.id} className="list-group-item">
-            {value.id} {value.name} {value.price}
+            {value.id} {value.name} {value.price}{" "}
             <button
               type="button"
               className="btn btn-primary"
               onClick={() => deleteProduct(value.id)}
             >
               del
+            </button>{" "}
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => updateProduct(value.id)}
+            >
+              upd
             </button>
           </li>
         ))}
       </ul>
       <br />
-
       <input
         type="text"
         className="form-control"
@@ -55,6 +78,14 @@ function Products() {
       <button type="button" className="btn btn-primary" onClick={addProduct}>
         add product
       </button>
+      <br />
+      <br />
+      <input
+        type="text"
+        className="form-control"
+        value={changeProduct}
+        onChange={(e) => setChangeProduct(e.target.value)}
+      />
     </>
   );
 }
